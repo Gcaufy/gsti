e4d8042d5c66cb36ad0785a2ab1a66e92a198296
Gcaufy <gcaufy@gmail.com>
2018-11-04 21:31:47 +0800

    refactor: restructure

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-const git = require('./git/index');
-const ui = require('./ui/index');
-const actions = require('./actions');
-const helper = require('./helper');
-const chalk = require('chalk');
+#!/usr/bin/env node
 
-
-git.status().then(status => {
-  let choices = helper.getChoices(status);
-  let footer, header;
-  ui.render({
-    choices,
-    header,
-    footer,
-    actions
-  });
-}).catch(e => {
-  if (e.stderr) {
-    console.log(chalk.red(e.stderr));
-  } else {
-    console.log(e);
-  }
-});
-
+require('./lib/index');
